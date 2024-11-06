4d4deff7929b5ea780448c4f0c51b49a8aef39b4
R4nz5r <ragibshahriar43@gmail.com>
2024-11-06 18:15:58 +0600

    webhooks added

--- server/server.js
+++ server/server.js
@@ -1,19 +1,20 @@
-import "dotenv/config"
-import express from "express"
-import cors from "cors"
-import connectDB from "./config/mongodb.js"
-
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import connectDB from "./config/mongodb.js";
+import userRouter from "./routes/userRoutes.js";
 
 // app configuration
-const PORT = process.env.PORT || 4000
-const app = express()
-await connectDB()
+const PORT = process.env.PORT || 4000;
+const app = express();
+await connectDB();
 
 // middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 // API routes
-app.get('/',(req, res) => res.send("API WORKING"))
+app.get("/", (req, res) => res.send("API WORKING"));
+app.use("/api/user", userRouter);
 
-app.listen(PORT,()=>console.log("listening on port "+PORT));
+app.listen(PORT, () => console.log("listening on port " + PORT));
